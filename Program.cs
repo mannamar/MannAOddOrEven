4dc77ed788346cf039e789b167b9fafa65281d55
Amardeep Mann <amar2slick@gmail.com>
2022-10-20 11:12:10 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// Amardeep Mann
+// 10-20-22
+// Mini Challenge #6 - Odds or Evens
+// We will take an input and tell the user wether the number is odd or even. Then ask them to play again
+
+Console.Clear();
+
+bool wantsToPlay = true;
+
+while (wantsToPlay) {
+
+  Console.WriteLine("Hey bud. Give me a number, any number! And I'll tell you wether it's odd or even");
+
+  bool isValidNum = false;
+  int num = 0;
+
+  while(!isValidNum) {
+    string userInput = Console.ReadLine();
+    isValidNum = Int32.TryParse(userInput, out num);
+    if (!isValidNum) {
+      Console.WriteLine("Please enter a valid number");
+    }
+  }
+
+  if (num % 2 == 0) {
+    Console.WriteLine($"\n{num} is even!");
+  } else {
+    Console.WriteLine($"\n{num} is odd!");
+  }
+
+  string response;
+
+  Console.WriteLine("");  // Line break for readability
+
+  do {
+    Console.WriteLine("Do you want to play again? (Please enter \"yes\" or \"no\")");
+    response = Console.ReadLine().ToLower();
+    if (response == "no" || response == "n") {
+      wantsToPlay = false;
+      Console.WriteLine("\nSee ya next time!");
+    } else {
+      Console.WriteLine(""); // Line break for readability
+    }
+  } while (response != "yes" && response != "no" && response != "y" && response != "n");
+
+}
